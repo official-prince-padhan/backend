fd455f5d6369394c2850b447d23665b375a4ba89
Prince Kumar <100506280+official-prince-padhan@users.noreply.github.com>
2025-04-21 23:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
 };
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-frontend.vercel.app'],
+  origin: ['http://localhost:3000', 'https://www.ptu-examresult.in'],
   credentials: true,
 }));app.use(express.json());
 
